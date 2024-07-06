d5d8fbc50c9bf69d3a0b68e0326a1f32d678699d
dima shelpuk <busikfaleot@gmail.com>
2024-07-06 18:45:11 +0300

    send form on server

--- expressapp/app.js
+++ expressapp/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+   
 const app = express();
- 
-app.use(function (_, response) {
-  response.sendFile(__dirname + "/index.html");
+   
+// создаем парсер для данных application/x-www-form-urlencoded
+const urlencodedParser = express.urlencoded({extended: false});
+  
+app.get("/", function (_, response) {
+    response.sendFile(__dirname + "/index.html");
 });
- 
-app.listen(3000);
+app.post("/", urlencodedParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.send(`${request.body.userName} - ${request.body.userAge}`);
+});
+   
+app.listen(3000, ()=>console.log("Сервер запущен..."));
