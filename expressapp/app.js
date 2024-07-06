dc270b8b8395831cc82d9e2489747aaa4f367c59
dima shelpuk <busikfaleot@gmail.com>
2024-07-06 17:56:34 +0300

    send fael

--- expressapp/app.js
+++ expressapp/app.js
@@ -1,16 +1,8 @@
 const express = require("express");
- 
 const app = express();
-app.get("/", function(request, response){
-     
-    response.send("<h1>Главная страница</h1>");
+ 
+app.use(function (_, response) {
+  response.sendFile(__dirname + "/index.html");
 });
-app.get("/about", function(request, response){
-     
-    response.send("<h1>О сайте</h1>");
-});
-app.get("/contact", function(request, response){
-     
-    response.send("<h1>Контакты</h1>");
-});
+ 
 app.listen(3000);
