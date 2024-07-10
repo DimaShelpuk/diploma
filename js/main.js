aa26fcc1c2603808d4d10f433657d39556077267
DimaShelpuk <busikfaleot@gmail.com>
2024-07-10 21:47:40 +0300

    promise

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-sdf,;sdf;sdmf s
+const promise1 = new Promise((resolve) => {
+    setTimeout(resolve, 500, "Hello");
+});
+const promise2 = new Promise((resolve) => {
+    setTimeout(resolve, 100, "World");
+});
+Promise.race([promise1, promise2])
+    .then(value => console.log(value))       // Hello
+    .catch(error => console.log(error));
