82be6bbe6d1d059a1ab2d3b401fe4565ee32dc5d
DimaShelpuk <busikfaleot@gmail.com>
2024-07-22 22:35:00 +0300

    delete appexress

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ form.addEventListener('submit', saveArticle);
 function saveArticle (event) {
     event.preventDefault();
     const myFormData = new FormData(form);
-    for (let data of myFormData) {
-        console.log(data);
-    }
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:3000");
+    xhr.send(myFormData);
+    
 }
