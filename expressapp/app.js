64aa0a426f4a6931c65039fe8a43d19dc6359e7e
dima shelpuk <busikfaleot@gmail.com>
2024-08-19 22:32:03 +0300

    autentification

--- expressapp/app.js
+++ expressapp/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require("mysql2");
+const { format, parseISO } = require('date-fns');
 const app = express();
 app.set('view engine', 'ejs');
 const PORT = 3000;
@@ -11,8 +12,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('style'));
 app.use(express.static('js'));
 
+const users = [
+    { email: 'user@example.com', password: 'potyer29'} // Пример пользователя
+];
+
 app.get("/", function (_, response) {
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/admin.html");
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    // Проверка данных пользователя
+    const user = users.find(u => u.email === email && u.password === password);
+    
+    if (user) {
+        // Успешная аутентификация
+        res.sendFile(__dirname + "/index.html");
+    } else {
+        // Неудачная аутентификация
+        res.send('Неверная электронная почта или пароль.');
+    }
 });
 
 // Обрабатываем POST-запросы на /check-user
@@ -74,45 +94,71 @@ app.post('/check-user', (req, res) => {
     res.render('app',{userData});
 });
 
-app.get('/page/:id', function (req, res) => {
-    const id = req.params.id;
-        
+app.get('/page/:id', function (req, res) {
+    const id = parseInt(req.params.id);
+    
+    console.log(id);
     const connection = mysql.createConnection({
         host: "localhost",
         user: "root",
         database: "test",
         password: "potyer29061934F"
-        });
-    
-    // тестирование подключения
+    });
     
     connection.connect(function(err){
         if (err) {
             return console.error("Ошибка: " + err.message);
         }
-        else{
         console.log("Подключение к серверу MySQL для получения данных успешно установлено");
-        }
     });
 
     connection.query('SELECT * FROM users WHERE id = ?', [id], (err, results) => {
         if (err) {
-            res.status(500).send('Error fetching data');
-            return;
+            connection.end();
+            return res.status(500).send('Error fetching data');
         }
-    });
-    
-    // закрытие подключения
-    connection.end(function(err) {
-        if (err) {
-            return console.log("Ошибка: " + err.message);
+        
+        if (results.length === 0) {
+            connection.end();
+            return res.status(404).send('User not found');
         }
-        console.log("Подключение закрыто");
-    });
-    
-    res.render('app',{userData})
 
+        const user = results[0];
+        
+        function dateTransform(str) {
+            let date = new Date(str);
+            return format(date, 'yyyy-MM-dd');
+        }
+        
+        const dateArray = [dateTransform(user.dateTB), dateTransform(user.dateTBNext), dateTransform(user.dateMed),  dateTransform(user.dateMedNext)];
+        
+        const userData = {
+            userName: user.name,
+            userPhone: user.worktype,
+            discharge: user.dicharge,
+            electroGroup: user.electroGroup,
+            workshop: user.workshop,
+            email: user.email,
+            dateTB: dateArray[0],
+            dateTBNext: dateArray[1],
+            dateMed: dateArray[2],
+            dateMedNext: dateArray[3],
+            userTalon: user.userTalon
+        };
+        
+        console.log(userData);
+        
+        connection.end(function(err) {
+            if (err) {
+                return console.log("Ошибка: " + err.message);
+            }
+            console.log("Подключение закрыто");
+        });
+
+        res.render('app', { userData });
+    });
 });
+
 
 
 // Запускаем сервер
